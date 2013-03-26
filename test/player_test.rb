a3cab9053f8fb027e264bfb5817f9c970bdf6d85
Dario D'Aprile <dario.daprile@gmail.com>
2013-03-26 00:07:07 +0000

    monopoly TDD

--- test/player_test.rb
+++ test/player_test.rb
@@ -2,7 +2,7 @@ require "minitest/autorun"
 require "./lib/player"
 
 class PlayerTest < MiniTest::Unit::TestCase
-	
+
 	def setup
 		@player = Player.new(1000, "Ben")
 	end
@@ -11,26 +11,26 @@ class PlayerTest < MiniTest::Unit::TestCase
 		@player.pay_rent 200
 		assert_equal 800, @player.balance
 	end
-	
+
 	def test_cant_afford_rent
 		@player.pay_rent 2000
-		assert_equal 0, @player.balance 
+		assert_equal 0, @player.balance
 	end
 
 	def test_own_land_when_land_bought
 		@player.buy_land "Mayfair", 800
-		assert_equal true, @player.owns_land?("Mayfair")
+		assert @player.owns_land?("Mayfair")
 	end
 
 	def test_balance_updated_when_land_bought
 		@player.buy_land "Mayfair" , 800
 		assert_equal 200, @player.balance
 	end
-	
+
 	def test_cant_afford_land
 		@player.buy_land "Mayfair", 1200
 		assert_equal 1000, @player.balance
-		assert_equal false, @player.owns_land?("Mayfair")
+		refute @player.owns_land?("Mayfair")
 	end
 
 	def test_can_own_much_land
