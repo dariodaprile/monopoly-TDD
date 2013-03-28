57cec2b954674ac079653685ec250d7ef2628ce0
Dario D'Aprile <dario.daprile@gmail.com>
2013-03-28 17:47:14 +0000

    Player roll dice and is able to buy land or pay rent

--- lib/engine.rb
+++ lib/engine.rb
@@ -11,10 +11,13 @@ class Engine
   def initialize
     @player1 = Player.new("Sam")
     @board = Board.new
-    # @land1 = Land.new(100, 1000, "Mayfair", 10)
+    @dice = Dice.new
+    @move = Move.new
+
+
     puts @player1.name
-
-
+    dice = @dice.roll
+    @move.update_position(@player1,dice)
 
   # def turn(player1)
   # end
@@ -25,17 +28,19 @@ class Engine
   # not to test: if player.position == tile.position
 
   if (@board.land_at(@player1.position)).available
-    puts "Would you like to buy #{@board.land_at(@player1.position).name}"
+    puts "Would you like to buy #{@board.land_at(@player1.position).name} for #{@board.land_at(@player1.position).value} pounds?"
     responce = gets.chomp.downcase
     if responce == "yes"
       @player1.buy_land(@board.land_at(@player1.position))
       puts "You now own:"
       @player1.land_list
+      puts "Your balance is now #{@player1.balance}"
     else
       puts "you did not buy the land"
     end
   else
-     puts "pay_rent"
+     @player1.pay_rent(@board.land_at(@player1.position).rent)
+     puts "you have paid #{@board.land_at(@player1.position).rent} of rent"
   end
 end
 
